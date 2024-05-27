1a9656035f8138068cf8e1ba0aa179078612ade1
YV97SL <nagesh.bhosle@ing.com>
2024-05-28 01:21:35 +0200

    Run spotless maven apply command

--- src/test/java/com/abnambro/project/recipe/TestRecipeServiceApplication.java
+++ src/test/java/com/abnambro/project/recipe/TestRecipeServiceApplication.java
@@ -1,11 +1,6 @@
 package com.abnambro.project.recipe;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.test.context.TestConfiguration;
-import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
-import org.springframework.context.annotation.Bean;
-import org.testcontainers.containers.PostgreSQLContainer;
-import org.testcontainers.utility.DockerImageName;
 
 public class TestRecipeServiceApplication {
     public static void main(String[] args) {
